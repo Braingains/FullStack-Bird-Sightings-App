92a5c63405bc78e59c54975ebf41695962bfcef9
ange47rm <ange47rm@hotmail.com>
2021-01-19 16:31:36 +0000

    MVP and extensions completed

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -42,7 +42,22 @@ const createRouter = function (collection) {
       res.json({ status: 500, error: err });
     });
   });
+
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+    .insertOne(newData)
+    .then( (result) => {
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+  });
+});
   return router;
 };
+
 
 module.exports = createRouter;
